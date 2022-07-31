98746fdf9574b3833dfb2e95126f7047d6c8bd78
JulieHock <julienhock@gmail.com>
2022-07-31 13:10:42 -0400

    updated files

--- Analysis_Project_Folder/Pewlet-Hackard-Analysis_Folder/Queries/Employee_Database_Challenge.sql
+++ Analysis_Project_Folder/Pewlet-Hackard-Analysis_Folder/Queries/Employee_Database_Challenge.sql
@@ -14,14 +14,14 @@ FROM retirement_titles
 WHERE to_date='9999-01-01'
 ORDER BY emp_no, to_date DESC;
 
---retrieve teh number of employees by their most recent job title
+--retrieve the number of employees by their most recent job title
 SELECT COUNT(title), title
 INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
 
---Create Mentorship elgibility table
+--Create Mentorship eligibility table
 SELECT DISTINCT ON (e.emp_no)e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
 INTO membership_eligibility
 FROM employees AS e
@@ -31,4 +31,26 @@ INNER JOIN titles AS t
 	ON (e.emp_no=t.emp_no)
 Where (de.to_date='9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY e.emp_no;
+
+--Get a count of titles who are eligible for mentorship program
+SELECT COUNT(title), title
+FROM membership_eligibility
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+--Expanding the mentorship eligible beyond 1965
+SELECT DISTINCT ON (e.emp_no)e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+INTO membership_eligibility_expanded
+FROM employees AS e
+INNER JOIN dept_emp AS de
+	ON (e.emp_no=de.emp_no)
+INNER JOIN titles AS t
+	ON (e.emp_no=t.emp_no)
+Where (de.to_date='9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1980-12-31')
+ORDER BY e.emp_no;
+--Count by title on membership_eligibility_expanded
+SELECT COUNT(title), title
+FROM membership_eligibility_expanded
+GROUP BY title
+ORDER BY COUNT(title) DESC;
 
